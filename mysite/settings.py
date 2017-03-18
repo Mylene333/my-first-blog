d8d1850b4a43ccf086195819d502b3942785461a
mylene333 <mylene.meyssonnier@wanadoo.fr>
2017-03-18 16:33:26 +0100

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jlx=2-t6tix2nh^@vgh(y1)fmij*$j++zl9v1t*%#sdj%*eg$h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
